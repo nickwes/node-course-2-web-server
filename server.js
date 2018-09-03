a6199e97ef64463c3d2fb6a6d2799e8c13e3b093
nickwes <nicholas@icon.co.za>
2018-09-03 14:17:35 +0200

    Setup start script and Heroku Settings

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -66,7 +68,6 @@ res.send({
 });
 
 
-
-app.listen(3000, () => {
-    console.log('Server is up');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
